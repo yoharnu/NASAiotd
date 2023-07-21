85535f479aa6afda422f3bdb8db244c3487b4947
yoharnu <dani91@gmail.com>
2023-07-21 15:22:05 -0400

    Add project files.

---
+++ NASAResponse.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NASAiotd
+{
+    internal class NASAResponse
+    {
+        public Hits? hits { get; set; }
+        internal class Hits
+        {
+            public Hit[]? hits { get; set; }
+            internal class Hit
+            {
+                public Source? _source { get; set; }
+                public Source? getSource() => _source;
+                internal class Source
+                {
+                    [JsonPropertyName("master-image")]
+                    public Image? masterImage { get; set; }
+
+                    internal class Image
+                    {
+                        public String? uri { get; set; }
+                        public String? title { get; set; }
+
+                    }
+                }
+            }
+        }
+
+        public Hits.Hit.Source.Image? getImage()
+        {
+            if (this.hits == null)
+            {
+                Console.WriteLine("first hits is null");
+            }
+            else if (this.hits.hits is null)
+            {
+                Console.WriteLine("second hits is null");
+            }
+            else if (this.hits.hits.Length == 0)
+            {
+                Console.WriteLine("second hits is empty");
+            }
+            else if (this.hits.hits[0].getSource() is null)
+            {
+                Console.WriteLine("source is null");
+            }
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            else if (this.hits.hits[0].getSource().masterImage is null)
+            {
+                Console.WriteLine("image is null");
+            }
+            else
+            {
+                return this.hits.hits[0].getSource().masterImage;
+            }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using NASAiotd;
+using System.Text.Json;
+
+internal class Program
+{
+    public static readonly HttpClient httpClient = new HttpClient();
+    private static async Task Main(string[] args)
+    {
+        //HttpClient httpClient = new HttpClient();
+
+        HttpRequestMessage request = new HttpRequestMessage(new HttpMethod("GET"), "https://www.nasa.gov/api/2/ubernode/_search?size=1&from=0&sort=promo-date-time%3Adesc&q=((ubernode-type%3Afeature%20OR%20ubernode-type%3Aimage)%20AND%20(routes%3A1446))&_source_include=promo-date-time%2Cmaster-image%2Cnid%2Ctitle%2Ctopics%2Cmissions%2Ccollections%2Cother-tags%2Cubernode-type%2Cprimary-tag%2Csecondary-tag%2Ccardfeed-title%2Ctype%2Ccollection-asset-link%2Clink-or-attachment%2Cpr-leader-sentence%2Cimage-feature-caption%2Cattachments%2Curi");
+
+        HttpResponseMessage response = await httpClient.SendAsync(request);
+        string text = await response.Content.ReadAsStringAsync();
+
+        //Console.WriteLine(text);
+        NASAResponse? NResponse = JsonSerializer.Deserialize<NASAResponse>(text);
+        if (NResponse is not null)
+        {
+            if (NResponse.getImage() is null)
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                if (NResponse.getImage().uri is null)
+                {
+                    Console.WriteLine("URI is null");
+                }
+                else
+                {
+                    string URL = NResponse.getImage().uri.Replace("public://", "https://www.nasa.gov/sites/default/files/");
+                    Wallpaper.Set(new Uri(URL), Wallpaper.Style.Fill);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
